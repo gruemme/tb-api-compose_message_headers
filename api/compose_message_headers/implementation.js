2f2745d52a6a0b64afad5c6e2001aa92c640a9f3
Chris G <12830070+gruemme@users.noreply.github.com>
2020-10-22 14:54:50 +0200

    Initial working version

---
+++ api/compose_message_headers/implementation.js
@@ -0,0 +1,44 @@
+/* eslint-disable object-shorthand */
+var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
+var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+var composeMessageHeaders = class extends ExtensionCommon.ExtensionAPI {
+  getAPI(context) {
+    context.callOnClose(this);
+
+    function getTabWindowFromTabId(tabId) {
+      let tabObject = context.extension.tabManager.get(tabId);
+      let realTab = tabObject.nativeTab;
+
+      return tabObject.window;
+    }
+
+    return {
+      composeMessageHeaders: {
+        addComposeHeader(tabId, headerName, headerValue) {
+          let realTabWindow = getTabWindowFromTabId(tabId);
+          realTabWindow.gMsgCompose.compFields.setHeader(headerName, headerValue);
+          console.log("Added " + headerName + ": " + headerValue + "to message headers");
+        },
+        deleteComposeHeader(tabId, headerName) {
+          let realTabWindow = getTabWindowFromTabId(tabId);
+          realTabWindow.gMsgCompose.compFields.deleteHeader(headerName);
+          console.log("Removed " + headerName + " from message headers");
+        },
+        hasComposeHeader(tabId, headerName) {
+          let realTabWindow = getTabWindowFromTabId(tabId);
+          return realTabWindow.gMsgCompose.compFields.hasHeader(headerName);
+        },
+        getComposeHeader(tabId, headerName) {
+          let realTabWindow = getTabWindowFromTabId(tabId);
+          return realTabWindow.gMsgCompose.compFields.getHeader(headerName);
+        },
+      },
+    };
+  }
+
+  close() {
+    console.log("Unloaded compose message header API!");
+    Services.obs.notifyObservers(null, "startupcache-invalidate", null);
+  }
+};
